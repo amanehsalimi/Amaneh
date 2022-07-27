3747e70138e380f0c4ee22ec512a8f396a839a4c
amaneh <amanehsalimi8@gmail.com>
2022-07-27 16:59:25 +0430

    creat project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.icu.text.DateIntervalFormat;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv);
+        tv.setText(new Date().toString());
+    }
+}
