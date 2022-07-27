7dc0536d9e49ada2f5757c272ec31642b5867182
amanehsalimi <104382322+amanehsalimi@users.noreply.github.com>
2022-07-27 17:37:56 +0430

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         TextView tv = findViewById(R.id.tv);
         tv.setText(new Date().toString());
 
